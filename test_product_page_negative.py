d128652f4ad7257239ca22ffefc21c579816a446
Igor2022ef <2023ief@gmail.com>
2023-04-04 14:02:15 +0300

    made marks xfail in param... and negative tests

--- test_product_page_negative.py
+++ test_product_page_negative.py
@@ -1,7 +1,9 @@
 import time
 from .pages.product_page import ProductObject
+import pytest
 
 # Отрицательные тесты
+@pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductObject(browser, link)
@@ -16,20 +18,20 @@ def test_guest_cant_see_success_message(browser):
     page.open()
     page.should_not_be_success_message()
 
+@pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductObject(browser, link)
     page.open()
     page.add_to_basket()
+
     """
     Товар из корзины сам не пропадает, поэтому в чистом виде срабатываение assert в этом методе не увидеть, 
     если раскомментить строку ниже, то будет падать из-за тогт, что товар в корзине, AssertionError увидим
+    и еще увидим метку XPASS
     """
-
     # page.solve_quiz_and_get_code()
     page.should_disappeared_message()
-
-
 
 
 #Для запуска в фаерфокс pytest -s --browser_name=firefox test_product_page.py
--- test_product_page_parametr.py
+++ test_product_page_parametr.py
@@ -2,6 +2,7 @@ import pytest
 from .pages.product_page import ProductObject
 
 #Код срабатывает только если в методе solve_quiz_and_get_code(self) закоментить try и все, что ниже
+
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -9,7 +10,7 @@ from .pages.product_page import ProductObject
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",marks=pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser, link):
